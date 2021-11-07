93f4dc32ee53da603ffab55c761328e3ec6c616a
nabeomki <beomki69@gmail.com>
2021-11-07 21:43:38 +0900

    POC API

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Movie {
+    id: String!
+    title: String!
+    year: Int
+  }
+
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "wow",
+    movies: () => [],
+    movie: () => ({
+      id: "123",
+      title: "hi",
+      year: 2021,
+    }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
